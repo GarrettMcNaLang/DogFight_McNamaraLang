2deac34ba513c9d5c65e21981b3b8667c6f7d492
GarrettMcNaLang <112025126+GarrettMcNaLang@users.noreply.github.com>
2024-10-20 20:11:06 -0400

    FinalBugsOnBuilding

--- Assets/Scripts/FighterScript.cs
+++ Assets/Scripts/FighterScript.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEditor;
-using UnityEditor.Experimental.GraphView;
+
 using UnityEngine;
 
 public class FighterScript : EnemyBehavior, IObjectPoolNotifier
